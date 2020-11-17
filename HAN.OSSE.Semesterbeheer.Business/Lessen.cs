2b903fba043c3cbbdc06087d5a558a02107615e5
Harm Lamers <HMA.Lamers@student.han.nl>
2020-11-17 18:52:18 +0100

    Created classes from classdiagram

---
+++ HAN.OSSE.Semesterbeheer.Business/Lessen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HAN.OSSE.Semesterbeheer.Model;
+
+namespace HAN.OSSE.Semesterbeheer.Business
+{
+    public class Lessen
+    {
+        public void CreateLes(string semester, string lesgegevens)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ValidateForm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateLes()
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO: Enable after implement Semester: 
+        //public void DeleteLessen(Semester semester)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public List<Les> GetLessenFromLeereenheid(Leereenheid leereenheid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
