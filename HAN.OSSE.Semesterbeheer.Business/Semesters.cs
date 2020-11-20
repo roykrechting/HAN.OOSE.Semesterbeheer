66ae280a5eb59ef495bfaaf0f7c56943f9ad17b9
Roy Krechting <rajkrechti@student.han.nl>
2020-11-20 13:45:31 +0100

    Add Semsters

---
+++ HAN.OSSE.Semesterbeheer.Business/Semesters.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HAN.OSSE.Semesterbeheer.Model;
+
+namespace HAN.OSSE.Semesterbeheer.Business
+{
+    public class Toetsen
+    {
+    }
+}
